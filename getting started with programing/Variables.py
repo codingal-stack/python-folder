9f08c590b30b2d4dc13f0c2108ed210da2147dca
codingal-stack <rkiqafco@gmail.com>
2024-10-05 12:04:17 +0300

    codingal work2

---
+++ getting started with programing/Variables.py
@@ -0,0 +1,7 @@
+A = 435
+B = 544
+C = A + B
+
+print("C is equal to \n",C ,  end= " ")
+print("Units is ABC \n")
+print("ABC is units" )
---
+++ getting started with programing/welcome.py
@@ -0,0 +1,7 @@
+print("welcome to programming")
+
+A = 543
+print(A)
+print("A is equal to \n",A)
+print("I am learning print statement",end = " ")
+print("I am learning pyhton")
