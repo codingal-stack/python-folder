7c5c8bfa9623fe8e78983d1189669e4c2b3872a9
codingal-stack <rkiqafco@gmail.com>
2024-10-12 11:58:42 +0300

    python operators 1

---
+++ python operators 1/average.py
@@ -0,0 +1,8 @@
+tree1 = 828888
+tree2 = 58375935
+tree3 = 242356346456
+tree4 = 49472563
+tree5 = 4457658787
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+average = sum/5
+print("average is",average)
---
+++ python operators 1/percentage.py
@@ -0,0 +1,6 @@
+maths = int(input("Enter your marks"))
+sci = int(input("Enter your marks"))
+lit = int(input("Enter your marks"))
+qh = int(input("Enter your marks"))
+percentage = maths + sci + lit + qh/4
+print("your percentage is ",percentage)
