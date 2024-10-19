a3caa3148d209d755b48bddca062cc35314943eb
codingal-stack <rkiqafco@gmail.com>
2024-10-19 12:07:08 +0300

    operators2

---
+++ pythinoperator/pythonoperator2.py
@@ -0,0 +1,12 @@
+A = 10
+B = -10
+C = 0
+
+if A  > C   and   A  > B:
+    print("A is greater than c and b")
+else:
+    print("one of the  numbers is not greater than b")
+if A  > C   or  A  > B:
+    print("a is greater than b or c")
+else:
+    print("a is not greater than b or c")
