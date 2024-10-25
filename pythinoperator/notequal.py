e6047649aa83f2e8166bc458cb8c72557073cea3
codingal-stack <rkiqafco@gmail.com>
2024-10-25 14:34:41 +0300

    op2

---
+++ pythinoperator/notequal.py
@@ -0,0 +1,15 @@
+a = 5
+b = 7
+c = 7
+print(a!=b)
+print(b!=c)
+
+str1 = "python"
+str2 = "coding"
+if (str1 != str2):
+    print(str1,"is not equal to",str2)
+
+
+a = 11
+if (a % 2  != 0):
+    print('a is not an even number')
