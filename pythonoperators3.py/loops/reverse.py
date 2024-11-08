8559d7298602dced415633c6c464b9536600a98b
codingal-stack <rkiqafco@gmail.com>
2024-11-08 14:06:41 +0300

    loops

---
+++ pythonoperators3.py/loops/reverse.py
@@ -0,0 +1,5 @@
+string = input('enter your own string:')
+reverse = ''
+for i in string :
+    reverse = i + reverse 
+print(' the reverse of the give string is ',reverse)
---
+++ pythonoperators3.py/loops/sumofwholenumbers.py
@@ -0,0 +1,6 @@
+#sum of whole no.
+n = int(input('enter number'))
+sum = 0
+for i in range(0,n+1):
+    sum = sum+i
+print('the sum of sum is ',sum)
