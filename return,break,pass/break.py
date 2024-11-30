364df2e54952e510e4f1ce442340cf34fe8cc34a
codingal-stack <rkiqafco@gmail.com>
2024-11-30 11:50:17 +0300

    return,pass,continue

---
+++ return,break,pass/break.py
@@ -0,0 +1,8 @@
+a = input('enter a word')
+for i in a:
+    if i.upper() == 'A':
+        print('alphabet A is present')
+        break
+else:
+    print('A not found')
+
---
+++ return,break,pass/continue.py
@@ -0,0 +1,7 @@
+codingal = 10
+
+for i  in range(codingal,0,-1):
+    if i == 5:
+        continue
+    print(i)
+
